cdd71da090bf362c30cbbfd3c9cb566792f1978d
shinuamai <laurad00sandovalg@gmail.com>
2024-01-02 15:07:52 -0500

    new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GiphyComponent } from './giphy/giphy.component';
 import { SearchComponent } from './search/search.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'inicio', component: HomeComponent},
   { path: 'giphy', component: GiphyComponent },
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: '**', redirectTo: '/pagenotfound', pathMatch: 'full'},
+  { path: 'pagenotfound', component: NotfoundComponent }
 ];
 
 @NgModule({
