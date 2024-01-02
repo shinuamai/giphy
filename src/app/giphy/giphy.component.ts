607c7f229e07dd0f56a45f3203e6b5ad67322ae0
shinuamai <laurad00sandovalg@gmail.com>
2024-01-02 08:59:55 -0500

    pages

---
+++ src/app/giphy/giphy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-giphy',
+  templateUrl: './giphy.component.html',
+  styleUrls: ['./giphy.component.css']
+})
+export class GiphyComponent {
+
+}
