31780275320aa666f2e87926005b224804c2e70b
shinuamai <laurad00sandovalg@gmail.com>
2024-01-02 17:10:10 -0500

    new change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GiphyService } from '../services/giphy.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  giphyWelcome = ''
+  secondGiphy = ''
+  constructor(private giphyService: GiphyService) { }
+  ngOnInit() {
+    this.getGiphies()
+  }
 
+  getGiphies(){
+    this.giphyService.getById('l4JyOCNEfXvVYEqB2').subscribe(giphies => {
+      this.giphyWelcome = giphies.data.images.original.url;
+    });
+    this.giphyService.getById('eXTue7sCt6ZvG').subscribe(giphies => {
+      this.secondGiphy = giphies.data.images.original.url;
+    });
+    
+  }
 }
--- src/app/services/giphy.service.ts
+++ src/app/services/giphy.service.ts
@@ -7,8 +7,7 @@ import { Giphy } from '../interfaces/giphy';
   providedIn: 'root'
 })
 export class GiphyService {
-  private baseUrl = 'https://api.giphy.com/v1/gifs/trending';
-  private urlSearch = 'https://api.giphy.com/v1/gifs/search'
+  private baseUrl = 'https://api.giphy.com/v1/gifs';
   private apiKey = 'ib0VTbUXjj5LlyrFP969nsWRxg3WrIpD';
   private limit = 12;
 
@@ -16,7 +15,7 @@ export class GiphyService {
   }
 
   getData(offset: number): Observable<any> {
-    const url = `${this.baseUrl}?api_key=${this.apiKey}&limit=${this.limit}&offset=${offset}&rating=g&bundle=messaging_non_clips`;
+    const url = `${this.baseUrl}/trending?api_key=${this.apiKey}&limit=${this.limit}&offset=${offset}&rating=g&bundle=messaging_non_clips`;
     return this.http.get(url)
       .pipe(
         catchError(error => {
@@ -27,7 +26,18 @@ export class GiphyService {
   }
 
   search(value: any, offset: number): Observable<any> {
-    const url = `${this.urlSearch}?api_key=${this.apiKey}&q=${value}&limit=${this.limit}&offset=${offset}&rating=g&lang=es&bundle=messaging_non_clips`;
+    const url = `${this.baseUrl}/search?api_key=${this.apiKey}&q=${value}&limit=${this.limit}&offset=${offset}&rating=g&lang=es&bundle=messaging_non_clips`;
+    return this.http.get(url)
+      .pipe(
+        catchError(error => {
+          console.error('Ocurrió un error en la solicitud HTTP GET:', error);
+          return throwError(() => error);
+        })
+      );
+  }
+
+  getById(giphyId:string):Observable<any> {
+    const url = `${this.baseUrl}/${giphyId}?api_key=${this.apiKey}&rating=g`;
     return this.http.get(url)
       .pipe(
         catchError(error => {
