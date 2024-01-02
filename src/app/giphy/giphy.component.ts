0933ee3a74308d886b2dea95a14a2d6fd38e217a
shinuamai <laurad00sandovalg@gmail.com>
2024-01-02 10:16:38 -0500

    implement of service

--- src/app/giphy/giphy.component.ts
+++ src/app/giphy/giphy.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GiphyService } from '../services/giphy.service';
+import { Giphy } from '../interfaces/giphy';
 
 @Component({
   selector: 'app-giphy',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./giphy.component.css']
 })
 export class GiphyComponent {
+  giphyAll: Giphy[] = []
+  constructor(private giphyService: GiphyService) { }
+
+  ngOnInit() {
+    this.getPosts()
+  }
+  getPosts() {
+    this.giphyService.getData().subscribe(giphy => {
+      this.giphyAll = giphy
+      console.log(this.giphyAll)
+    })
+  }
 
 }
